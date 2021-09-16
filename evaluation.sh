b59ba81097641f820b5fbc46b1ae39a23222eedc
Baikjin Jung <bjjung@postech.ac.kr>
2021-09-16 09:05:16 +0900

    Putting the right path and formatting

--- evaluation.sh
+++ evaluation.sh
@@ -1,37 +1,35 @@
 #!/bin/bash
 
-undo_BPE () {
-
-	sentencepiece_model=$1
-	hypothesis_file=$2
-	echo "Undoing BPE... (sentencepiece)"
-	spm_decode \
-		--model=${sentencepiece_model} \
-		--input_format=piece \
-		< ${hypothesis_file} > \
-		${hypothesis_file}.unBPE
-	echo "BPE undone..."
-
-	raw_hypothesis=${hypothesis_file}.unBPE
-	echo "Restoring special characters..."
-	cat ${raw_hypothesis} | sed 's/ TM / ™ /g' > ${raw_hypothesis}.special
-	cat ${raw_hypothesis}.special | sed 's/ ? / ? /g' > ${raw_hypothesis}.special
-	cat ${raw_hypothesis}.special | sed 's/′′/″/g' > ${raw_hypothesis}.special
-	mv ${raw_hypothesis}.special ${raw_hypothesis}
-	rm ${raw_hypothesis}.special
-	echo "All special characters have been restored!"
-
-}
-
 if [ $# -ne 5 ]; then
-  echo "Five arguments must be entered"
-  exit
+    echo "Five arguments must be entered"
+    exit
 fi
 hypothesis_path=$1
 reference_path=$2
 sentencepiece_model=$3
 output_directory=$4
 evaluation_result=$5
+
+undo_BPE () {
+
+    sentencepiece_model=$1
+    hypothesis_file=$2
+    raw_hypotheses=${output_directory}/raw_hypotheses
+    echo "Undoing BPE... (sentencepiece)"
+    spm_decode \
+        --model=${sentencepiece_model} \
+        --input_format=piece \
+        < ${hypothesis_file} > \
+        ${raw_hypotheses}
+    echo "BPE undone..."
+
+    echo "Restoring special characters..."
+    cat ${raw_hypotheses} | sed 's/ TM / ™ /g' > ${raw_hypotheses}
+    cat ${raw_hypotheses} | sed 's/ ? / ? /g' > ${raw_hypotheses}
+    cat ${raw_hypotheses} | sed 's/′′/″/g' > ${raw_hypotheses}
+    echo "All special characters have been restored!"
+
+}
 
 script_directory=$( dirname $0 )
 BLEU_script=${script_directory}/multi-bleu.perl
@@ -41,15 +39,15 @@ undo_BPE ${sentencepiece_model} ${hypothesis_path}
 
 echo "TER scoring..."
 bash ${TER_script} \
-	-h ${hypothesis_path} \
-	-r ${reference_path} \
-	-o ${output_directory} \
-	-s ${evaluation_result}
+    -h ${hypothesis_path} \
+    -r ${reference_path} \
+    -o ${output_directory} \
+    -s ${evaluation_result}
 echo "TER scoring done..."
 
 echo "BLEU scoring..."
 perl ${BLEU_script} \
-	${reference_path} \
-	< ${hypothesis_path} > \
-	${output_directory}/${evaluation_result}.BLEU
+    ${reference_path} \
+    < ${hypothesis_path} > \
+    ${output_directory}/${evaluation_result}.BLEU
 echo "BLEU scoring done!"
--- plugins/TER/utilities.sh
+++ plugins/TER/utilities.sh
@@ -2,36 +2,36 @@
 
 measure_overlaps () {
 # count how many examples have 0 TER
-	count=0
-	TER=($(sed '6p' $1 | awk '{print $17}'))
-	for y in ${x[@]}; do
-		if [ ${y} == 0.000 ]; then
-			count=$((${count}+1))
-		fi
-	done
+    count=0
+    TER=( $( sed '6p' $1 | awk '{print $17}' ) )
+    for y in ${x[@]}; do
+        if [ ${y} == 0.000 ]; then
+            count=$( ( ${count}+1 ) )
+        fi
+    done
 echo ${count}
 }
 
 evaluate_models () {
 # when we have several model results
-	base_directory=$1
-	model_name_prefix=$2
-	hypothesis_file=$3
-	output_directory=$4
-	evaluation_result=$5
-	reference_path=$6
-	BPE_model_path=$7
+    base_directory=$1
+    model_name_prefix=$2
+    hypothesis_file=$3
+    output_directory=$4
+    evaluation_result=$5
+    reference_path=$6
+    BPE_model_path=$7
 
-	for evaluated_model in $base_directory/* ; do
-		model_name=$(basename $evaluated_model)
-		if [ -d ${evaluated_model} ] || \
+    for evaluated_model in ${base_directory}/* ; do
+        model_name=$( basename ${evaluated_model} )
+        if [ -d ${evaluated_model} ] || \
            [ ${model_name} == ${model_name_prefix}* ]; then
-			../../evaluation.sh \
-				${model_name}/${hypothesis_file} \
-				${reference_path} \
-				${BPE_model_path} \
-				${output_directory} \
-				${evaluation_result}
-		fi
-	done
+            $( dirname $( dirname $( dirname $0 ) ) )/evaluation.sh \
+                ${model_name}/${hypothesis_file} \
+                ${reference_path} \
+                ${BPE_model_path} \
+                ${output_directory} \
+                ${evaluation_result}
+        fi
+    done
 }
