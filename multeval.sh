66fb79e411b623fab1770265518cf6a8fe7d0f9c
aeceou <jbj0506@yonsei.ac.kr>
2022-01-16 03:32:52 +0900

    Correcting errors

--- multeval.sh
+++ multeval.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-if [[ ! -d "$( dirname $0 )/plugins/multeval" ]]; then
+current_directory=$( readlink -f $( dirname $0 ) )
+
+if [[ ! -d "${current_directory}/plugins/multeval" ]]; then
     git clone https://github.com/aeceou/multeval.git $( dirname $0 )/plugins/multeval
 fi
 
@@ -12,9 +14,12 @@ mt_baseline=$2
 # it can be the given raw MT outputs or the baseline APE system's hypothesis texts
 system_hypotheses=$3
 
-bash $( dirname $0 )/plugins/multeval/multeval.sh \
+multeval_directory=${current_directory}/plugins/multeval
+ln -s ${current_directory}/data ${multeval_directory}/data
+cd ${multeval_directory}
+bash ./multeval.sh \
     eval \
     --refs ${gold_standard} \
-    --hyps-baseline ${mt_baseline}* \
-    --hyps-sys1 ${system_hypotheses}* \
+    --hyps-baseline ${mt_baseline} ${mt_baseline} \
+    --hyps-sys1 ${system_hypotheses} ${system_hypotheses} \
     --metrics bleu ter length
